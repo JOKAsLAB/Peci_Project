6c2489d40dfe6555a4526af3e8b862951a59e85c
carloslf2258 <carlos.verenzuela@ua.pt>
2026-04-10 12:55:09 +0100

    Cleaning

--- backend/database/fix_admin_comment_column.sql
+++
@@ -1,2 +0,0 @@
--- Fix admin comment column name (should be admin_comment not admincomment)
-ALTER TABLE request RENAME COLUMN admincomment TO admin_comment;
--- backend/database/fix_request_table.sql
+++
@@ -1,2 +0,0 @@
--- Fix missing request_type column in Request table
-ALTER TABLE request ADD COLUMN request_type VARCHAR(20) NOT NULL DEFAULT 'other';
